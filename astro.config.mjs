1eaccae7467eb5a5bdb8e85be50b60d236a77417
Scott Odle <scott@sjodle.com>
2024-01-13 20:14:30 -0700

    add sitemaps, robots, light mode, staging of ground post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+const sitemapIgnoreList = [
+  'https://scoott.blog/posts/2024/01/ground/',
+];
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://scoott.blog',
+  integrations: [mdx(), sitemap({
+    filter: u => !sitemapIgnoreList.includes(u),
+  })]
+});
